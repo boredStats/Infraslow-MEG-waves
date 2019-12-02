ee63e2bf89f99cf7cac5e66cf3d1a9ce62f103ae
boredStats <ian.abenes@gmail.com>
2019-12-02 13:58:35 -0600

    update effective connectivity calculations

--- connectivity.py
+++ connectivity.py
@@ -25,7 +25,6 @@ def _try_epoching(dataset=None, fs=500, transpose=True):
     else:
         epoched_data = np.ndarray(shape=(n_splits, ncols, new_constant))
     for i, (_, ts) in enumerate(indexer.split(dataset)):
-        print(ts)
         segment = dataset[ts, :]
         if segment.shape[0] < new_constant:
             epoch = np.pad(
@@ -37,7 +36,7 @@ def _try_epoching(dataset=None, fs=500, transpose=True):
         if transpose is True:
             epoch = epoch.T
         epoched_data[i, :, :] = epoch
-        del epoch
+        del epoch, segment
     return epoched_data
 
 
@@ -86,10 +85,13 @@ def epoch_MEG(rois=None, fs=500, transpose=True):
     out.close()
 
 
-def effective_connectivity(start_rois):
+def effective_connectivity(start_rois=None):
     data_dir = utils.ProjectData.data_dir
     subjects, sessions = utils.ProjectData.meg_metadata
     glasser_rois = utils.ProjectData.glasser_rois
+
+    if start_rois is None:
+        start_rois = glasser_rois
 
     start_indices, end_indices, connections = [], [], []
     if type(start_rois) == str:
@@ -100,35 +102,36 @@ def effective_connectivity(start_rois):
                 idxs = [g] * len(glasser_rois)
                 start_indices.extend(idxs)
                 end_indices.extend(np.arange(len(glasser_rois)))
-                connections.append('%s %s' % (sr, gr))
+            connections.append('%s %s' % (sr, gr))
+
     start_indices = np.array(start_indices)
     indices = (start_indices, end_indices)
-    # epoch_file = os.path.join(data_dir, 'MEG_epoched.hdf5')
+
+    epoch_file = os.path.join(data_dir, 'MEG_epoched.hdf5')
+    res_dict = {}
     for sess in sessions:
+        res_df = pd.DataFrame(index=subjects, columns=connections)
         for subj in subjects:
-            key = subj + '/MEG/' + sess + '/timeseries'
-            print('Epoching %s' % str(key))
-            dataset = utils.index_hcp_raw(key=key)
-            epoch_data = _try_epoching(dataset)
-            del dataset
-            # f = h5py.File(epoch_file, 'r')
-            # epoch_data = f[subj][sess]['epochs'][...]
-            # f.close()
+            print('Calculating PSI for %s %s' % (sess, subj))
+            f = h5py.File(epoch_file, 'r')
+            epoched_data = f[subj][sess]['epochs'][...]
+            f.close()
             eff_con, _, _, _, _ = phase_slope_index(
-                data=epoch_data,
+                data=epoched_data,
                 indices=indices,
                 fmin=8,
                 fmax=12,
                 mode='fourier',
                 sfreq=500,
-                verbose=0,
+                verbose='CRITICAL',
             )
-            del epoch_data
-            print(eff_con)
-            break
-        break
+            res_df.loc[subj] = np.ndarray.flatten(eff_con)
+            del epoched_data, eff_con
+        res_dict[sess] = res_df
+    return res_dict
 
 
 if __name__ == "__main__":
-    # epoch_MEG()
-    effective_connectivity(start_rois=['p24pr_L', 'p24pr_R'])
+    res = effective_connectivity(start_rois=['p24pr_L', 'p24pr_R'])
+    outpath = os.path.join(data_dir, 'dACC_effective_connectivity.xlsx')
+    utils.save_xls(res, outpath)
